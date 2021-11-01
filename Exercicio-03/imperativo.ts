1992454d3f5a1e71b414e1200bc0a85eeba10347
Lucas Alexander <lucasalexander13@hotmail.com>
2021-11-01 15:41:06 -0300

    Mudança de i para indice, como já sugerido

--- Exercicio-03/imperativo.ts
+++ Exercicio-03/imperativo.ts
@@ -2,8 +2,8 @@ function listarValores (lista: any[]) : number[] {
     // criação da lista vazia para armazenar os valores
     let nova_lista: number[] = [];
     // percorrer a lista e filtrar os numeros para adicionar à nova_lista
-    for (let i: number = 0; i < lista.length; i++){
-        let item = Number(lista[i]);
+    for (let indice: number = 0; indice < lista.length; indice++){
+        let item = Number(lista[indice]);
         // checar se o item da lista é numérico
         if (isNaN(item)){
             continue;
@@ -16,15 +16,15 @@ function listarValores (lista: any[]) : number[] {
     let maior_valor: number = nova_lista[0];
     let menor_valor: number = nova_lista[0];
     // para cada número na lista, encontrar o maior e menor valor, e somar todos os valores
-    for (let i: number = 0; i < nova_lista.length; i++){
+    for (let indice: number = 0; indice < nova_lista.length; indice++){
 
-        if (nova_lista[i] > maior_valor) {
-            maior_valor = nova_lista[i];
+        if (nova_lista[indice] > maior_valor) {
+            maior_valor = nova_lista[indice];
         }
-        else if (nova_lista[i] < menor_valor) {
-            menor_valor = nova_lista[i];
+        else if (nova_lista[indice] < menor_valor) {
+            menor_valor = nova_lista[indice];
         }
-        somatoria += nova_lista[i];
+        somatoria += nova_lista[indice];
     }
     // calcular o valor médio a partir da quantidade de elementos na lista
     let valor_medio = somatoria / nova_lista.length;
