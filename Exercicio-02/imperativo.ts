597060681791f84c74059601ecbabe26cc8661e7
Lucas Alexander <lucasalexander13@hotmail.com>
2021-11-01 11:31:50 -0300

    Tipagem da lista como Array<Pessoa>

--- Exercicio-02/imperativo.ts
+++ Exercicio-02/imperativo.ts
@@ -1,5 +1,11 @@
+// declaração da interface para a lista
+interface Pessoa {
+    id: number,
+    name: string,
+    bio: string
+}
 // declaração da variável 
-let lista: Array<Object> = [
+let lista: Array<Pessoa> = [
     {"id" : 1, "name": "Ada Lovelace", "bio" : "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina"},
     {"id" : 2, "name": "Alan Turing", "bio" : "Alan Turing foi um matemático, cientista da computação, lógico, criptoananova_lista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificia"},
     {"id" : 3, "name": "Nikola Tesla", "bio" : "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada."},
@@ -11,7 +17,7 @@ let lista: Array<Object> = [
 ----------------------------------------------------------------------------------*/
 
 // retorna a Bio da lista a partir do id passado como parâmetro
-function retornarBio(lista: Array<Object>, id: number): string {
+function retornarBio(lista: Array<Pessoa>, id: number): string {
     let bio: string;
 
     for(let i: number = 0; i < lista.length; i++) {
@@ -22,7 +28,7 @@ function retornarBio(lista: Array<Object>, id: number): string {
     return bio;
 }
 // retorna o Name da lista a partir do id passado como parâmetro
-function retornarNome(lista: Array<Object>, id: number): string {
+function retornarNome(lista: Array<Pessoa>, id: number): string {
     let nome: string;
 
     for(let i: number = 0; i < lista.length; i++) {
@@ -33,8 +39,8 @@ function retornarNome(lista: Array<Object>, id: number): string {
     return nome;
 }
 //retorna uma nova lista, removendo os itens do id passado
-function apagarItem(lista: Array<Object>, id: number): Array<Object> {
-    let nova_lista: Array<Object> = lista;
+function apagarItem(lista: Array<Pessoa>, id: number): Array<Pessoa> {
+    let nova_lista: Array<Pessoa> = lista;
 
     for(let i: number = 0; i < nova_lista.length; i++) {
         if (nova_lista[i]['id'] === id) {
@@ -44,8 +50,8 @@ function apagarItem(lista: Array<Object>, id: number): Array<Object> {
     return nova_lista;
 }
 // retorna uma nova lista com as informações alteradas a partir do id e do tipo de informação
-function alterarInfo(lista: Array<Object>, id: number, tipo: string, nova_info: string): Array<Object> {
-    let nova_lista: Array<Object> = lista;
+function alterarInfo(lista: Array<Pessoa>, id: number, tipo: string, nova_info: string): Array<Pessoa> {
+    let nova_lista: Array<Pessoa> = lista;
 
     for(let i: number = 0; i < nova_lista.length; i++) {
         if (nova_lista[i]['id'] === id) {
