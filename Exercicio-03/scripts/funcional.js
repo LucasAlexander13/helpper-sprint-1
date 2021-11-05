5bbba04932a1d8511aef78303329cfc240c0ac12
Lucas Alexander <lucasalexander13@hotmail.com>
2021-11-05 10:26:42 -0300

    Atualização dos códigos tranpilados para JS

--- Exercicio-03/scripts/funcional.js
+++ Exercicio-03/scripts/funcional.js
@@ -2,11 +2,11 @@ function listarValores(lista) {
     // filtragem dos valores numéricos para uma nova lista
     const nova_lista = lista.filter(valor => !isNaN(valor)).map(Number);
     // filtra o menor número dentro da lista numérica e armazena no menor_valor
-    const menor_valor = nova_lista.reduce((valor, auxiliar) => Math.min(valor, auxiliar));
+    const menor_valor = nova_lista.reduce((valor, auxiliar) => Math.min(...nova_lista));
     // filtra o maior número dentro da lista numérica e armazena no maior_valor
-    const maior_valor = nova_lista.reduce((valor, auxiliar) => Math.max(valor, auxiliar));
+    const maior_valor = nova_lista.reduce((valor, auxiliar) => Math.max(...nova_lista));
     // criação de uma função que retorna a soma entre os valores recebidos
-    const somatorio = (valor, auxiliar) => { return valor + auxiliar; };
+    const somatorio = (valor, auxiliar) => valor + auxiliar;
     // realiza a soma de todos os valores e divide pela quantidade de valores
     const valor_medio = (nova_lista.reduce(somatorio, 0)) / nova_lista.length;
     // retorna a lista formatada nos padrões pedidos
