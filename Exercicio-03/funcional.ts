be504a35af771e332da4fb52c0c00c514dc92b91
Lucas Alexander <lucasalexander13@hotmail.com>
2021-11-06 11:57:39 -0300

    Correção dos métodos Math.min e Math.max

--- Exercicio-03/funcional.ts
+++ Exercicio-03/funcional.ts
@@ -2,9 +2,9 @@ function listarValores (lista: any[]) : number[] {
     // filtragem dos valores numéricos para uma nova lista
     const nova_lista: number[] = lista.filter(valor => !isNaN(valor)).map(Number);
     // filtra o menor número dentro da lista numérica e armazena no menor_valor
-    const menor_valor: number = nova_lista.reduce((valor, auxiliar) => Math.min(...nova_lista));
+    const menor_valor: number = Math.min(...nova_lista);
     // filtra o maior número dentro da lista numérica e armazena no maior_valor
-    const maior_valor: number = nova_lista.reduce((valor, auxiliar) => Math.max(...nova_lista));
+    const maior_valor: number = Math.max(...nova_lista);
     // criação de uma função que retorna a soma entre os valores recebidos
     const somatorio = (valor: number, auxiliar: number) => valor + auxiliar;
     // realiza a soma de todos os valores e divide pela quantidade de valores
